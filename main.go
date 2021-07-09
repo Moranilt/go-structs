b53445d3cfa0dd69bd19a852d38717272a10ebd8
d.berezhnoy <moranis.art.media@yandex.ru>
2021-07-09 18:40:58 +0300

    changed main func and logic for calling functions

--- main.go
+++ main.go
@@ -4,16 +4,35 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Moranilt/go-structs/models"
 	request "github.com/Moranilt/go-structs/request"
 )
 
-func initFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-	parseBody := request.ParsedBody(r)
-	w.Write(request.ToJSON(parseBody))
+func userController(w http.ResponseWriter, r *http.Request) {
+	parsedBody := models.User(r).ToJSON()
+	w.Write(parsedBody)
+}
+
+func locationController(w http.ResponseWriter, r *http.Request) {
+	parsedBody := models.Location(r).ToJSON()
+	w.Write(parsedBody)
+}
+
+func initHandler(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := request.Init(r).CheckMethod(http.MethodPost)
+		w.Header().Set("content-type", "application/json")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusMethodNotAllowed)
+			return
+		}
+		fn(w, r)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", initFunc)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/user", initHandler(userController))
+	mux.HandleFunc("/location", initHandler(locationController))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
